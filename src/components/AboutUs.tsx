dd336da5e6fbaf788eb8f90f6ee864c0bc834ed5
martin <guerreiromartin@gmail.com>
2025-09-01 11:48:06 -0300

    products added

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -6,19 +6,19 @@ const AboutUs = () => {
           <p className="mt-2 text-3xl leading-8 font-serif font-bold text-primary sm:text-4xl">
             ¿Quien soy?
           </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-600 lg:mx-auto">
-            Hace un par de años descubrí el maravilloso mundo de la pastelería y, desde entonces, no he parado de batir. 
-            Adoro cocinar tortas, masas y galletas. Me encanta diseñar formas, nuevas decoraciones y probar rellenos. 
+          <p className="mt-4 text-xl text-gray-600 lg:mx-auto">
+            Hace un par de años descubrí el maravilloso mundo de la pastelería y, desde entonces, no he parado de batir.
+            Adoro cocinar tortas, masas y galletas. Me encanta diseñar formas, nuevas decoraciones y probar rellenos.
           </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-600 lg:mx-auto">
-          Es un placer plasmar en mis productos los deseos y preferencias de mis clientes, y que mis preparaciones 
-          los dejen satisfechos y felices. Cocino para deleitarme y para que quienes compran mis productos también 
-          lo hagan.
-        </p>
-        <p className="mt-4 max-w-2xl text-xl text-gray-600 lg:mx-auto">
-          Elegí lo que más te guste y descubrí cómo estas delicias pueden alegrar tu día. ¡Contá conmigo 
-          para endulzar tus celebraciones!
-        </p>
+          <p className="mt-4 text-xl text-gray-600 lg:mx-auto">
+            Es un placer plasmar en mis productos los deseos y preferencias de mis clientes, y que mis preparaciones
+            los dejen satisfechos y felices. Cocino para deleitarme y para que quienes compran mis productos también
+            lo hagan.
+          </p>
+          <p className="mt-4 text-xl text-gray-600 lg:mx-auto">
+            Elegí lo que más te guste y descubrí cómo estas delicias pueden alegrar tu día. ¡Contá conmigo
+            para endulzar tus celebraciones!
+          </p>
         </div>
 
       </div>
