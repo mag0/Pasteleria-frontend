7918a03bc516175d3e2a2a0913bc55ed3f63bbf9
martin <guerreiromartin@gmail.com>
2025-09-02 18:34:43 -0300

    errors fixed

--- src/pages/app/Home.tsx
+++ src/pages/app/Home.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react';
+// import { useEffect } from 'react';
 import Hero from '../../components/Hero';
 import AboutUs from '../../components/AboutUs';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Testimonials from '../../components/Testimonials';
-import type { FeaturedProduct } from '../../interfaces/FeaturedProduct';
+// import type { FeaturedProduct } from '../../interfaces/FeaturedProduct';
 
 const Home = () => {
-  const [featuredProducts, setFeaturedProducts] = useState<FeaturedProduct[]>([]);
+  // const [featuredProducts, setFeaturedProducts] = useState<FeaturedProduct[]>([]);
 
-  useEffect(() => {
-    fetch('/json/featuredProducts.json')
-      .then(res => res.json())
-      .then(data => setFeaturedProducts(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch('/json/featuredProducts.json')
+  //     .then(res => res.json())
+  //     .then(data => setFeaturedProducts(data));
+  // }, []);
 
   return (
     <div className="bg-background">
